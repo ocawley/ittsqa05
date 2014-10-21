fdacee49e8cdd9ceb887da7f395835744215ef71
Leftfield <x00115168-grades.sh>
2014-10-21 13:26:53 +0100

    gradeslab

---
+++ x00115168-grades.sh
@@ -0,0 +1,35 @@
+#Test Case 1
+#Input 50 60 
+#Expected Output: Pass
+#Actual Output: Fail
+java grades 50 60 
+
+#Test Case 2
+#Input 50 80 
+#Expected Output: Pass
+#Actual Output: pass
+java grades 50 80
+
+#Test Case 3
+#Input 80 100 
+#Expected Output: Pass with destinction
+#Actual Output: Pass with distinction
+java grades 80 100
+
+#Test Case 4
+#Input 40 0  
+#Expected Output: Component fail
+#Actual Output: Component fail
+java grades 40 0
+
+#Test Case 5
+#Input 0 40 
+#Expected Output: Component fail
+#Actual Output: Component fail
+java grades 0 40
+
+#Test Case 6
+#Input 1100 899
+#Expected Output: Invailed input
+#Actual Output: Invailed input
+java grades 1100 899
